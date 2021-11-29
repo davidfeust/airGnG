62658c8e6f02b4f99bf2fccb7118a8a02e926491
hodefiDavid <73063199+hodefiDavid@users.noreply.github.com>
2021-11-30 00:18:05 +0200

    add Air_GnG icon

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
 import Routes from "./navigation";
-import { LogBox } from "react-native";
+import {LogBox} from "react-native";
 
 LogBox.ignoreLogs(["Setting a timer"]);
 LogBox.ignoreLogs(["AsyncStorage"]);
 
 export default function App() {
-  return <Routes />;
+    return <Routes/>;
+
 }
+
+
+
+
