324a6a88c020005faf0a13f14185d657d62f7d9f
achiyazigi <matanelcohen0@walla.com>
2021-12-05 01:59:47 +0200

    prettier tab change

--- components/CustomDatePicker.js
+++ components/CustomDatePicker.js
@@ -7,38 +7,41 @@ import TimeSlot from "./TimeSlot";
 import { globalStyles } from "../assets/styles/globalStyles";
 
 export default function CustomDatePicker() {
-  const removeTimeSlot = (toRemove) => {
-    const newVal = timeSlots.filter((ts) => ts != toRemove);
-    console.log(newVal);
-    setTimeSlots(newVal);
-  };
-  const [timeSlots, setTimeSlots] = useState([<TimeSlot />]);
+    const removeTimeSlot = (toRemove) => {
+        const newVal = timeSlots.filter((ts) => ts != toRemove);
+        console.log(newVal);
+        setTimeSlots(newVal);
+    };
+    const [timeSlots, setTimeSlots] = useState([<TimeSlot />]);
 
-  const addTimeSlot = () => {
-    setTimeSlots([...timeSlots, <TimeSlot />]);
-  };
+    const addTimeSlot = () => {
+        setTimeSlots([...timeSlots, <TimeSlot />]);
+    };
 
-  return (
-    <View>
-      {timeSlots.map((ts, idx) => (
-        <View key={idx} style={{ flexDirection: "row" }}>
-          {ts}
-          <TouchableOpacity onPress={() => removeTimeSlot(ts)}>
-            <MaterialCommunityIcons
-              name="minus-circle"
-              color={colors.primary}
-              size={30}
-            />
-          </TouchableOpacity>
+    return (
+        <View>
+            {timeSlots.map((ts, idx) => (
+                <View key={idx} style={{ flexDirection: "row" }}>
+                    {ts}
+                    <TouchableOpacity onPress={() => removeTimeSlot(ts)}>
+                        <MaterialCommunityIcons
+                            name="minus-circle"
+                            color={colors.primary}
+                            size={30}
+                        />
+                    </TouchableOpacity>
+                </View>
+            ))}
+            <TouchableOpacity
+                onPress={addTimeSlot}
+                style={{ alignSelf: "center" }}
+            >
+                <MaterialCommunityIcons
+                    name="plus-circle"
+                    color={colors.primary}
+                    size={30}
+                />
+            </TouchableOpacity>
         </View>
-      ))}
-      <TouchableOpacity onPress={addTimeSlot} style={{ alignSelf: "center" }}>
-        <MaterialCommunityIcons
-          name="plus-circle"
-          color={colors.primary}
-          size={30}
-        />
-      </TouchableOpacity>
-    </View>
-  );
+    );
 }
--- components/TimeSlot.js
+++ components/TimeSlot.js
@@ -5,59 +5,59 @@ import DatePicker from "react-native-neat-date-picker";
 import { colors } from "../assets/styles/colors";
 
 const TimeSlot = () => {
-  const [sdate, setSdate] = useState(null);
-  const [fdate, setFdate] = useState(null);
+    const [sdate, setSdate] = useState(null);
+    const [fdate, setFdate] = useState(null);
 
-  const [showDatePicker, setShowDatePicker] = useState(false);
+    const [showDatePicker, setShowDatePicker] = useState(false);
 
-  const dateToString = (date) => {
-    return date.getDate() + "/" + date.getMonth() + "/" + date.getYear();
-  };
+    const dateToString = (date) => {
+        return date.getDate() + "/" + date.getMonth() + "/" + date.getYear();
+    };
 
-  const openDatePicker = () => {
-    setShowDatePicker(true);
-  };
+    const openDatePicker = () => {
+        setShowDatePicker(true);
+    };
 
-  const onCancel = () => {
-    // You should close the modal in here
-    setShowDatePicker(false);
-  };
+    const onCancel = () => {
+        // You should close the modal in here
+        setShowDatePicker(false);
+    };
 
-  const onConfirm = (start, end) => {
-    // You should close the modal in here
-    setShowDatePicker(false);
+    const onConfirm = (start, end) => {
+        // You should close the modal in here
+        setShowDatePicker(false);
 
-    // The parameter 'date' is a Date object so that you can use any Date prototype method.
-    setSdate(start);
-    setFdate(end);
-  };
+        // The parameter 'date' is a Date object so that you can use any Date prototype method.
+        setSdate(start);
+        setFdate(end);
+    };
 
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        justifyContent: "space-between",
-      }}
-    >
-      <Text>
-        {sdate ? dateToString(sdate) : "start"}-
-        {sdate ? dateToString(fdate) : "finish"}
-      </Text>
-      <TouchableOpacity onPress={openDatePicker}>
-        <MaterialCommunityIcons
-          name="calendar"
-          color={colors.primary}
-          size={30}
-        />
-      </TouchableOpacity>
-      <DatePicker
-        onConfirm={onConfirm}
-        isVisible={showDatePicker}
-        mode={"range"}
-        onCancel={onCancel}
-      />
-    </View>
-  );
+    return (
+        <View
+            style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+            }}
+        >
+            <Text>
+                {sdate ? dateToString(sdate) : "start"}-
+                {sdate ? dateToString(fdate) : "finish"}
+            </Text>
+            <TouchableOpacity onPress={openDatePicker}>
+                <MaterialCommunityIcons
+                    name="calendar"
+                    color={colors.primary}
+                    size={30}
+                />
+            </TouchableOpacity>
+            <DatePicker
+                onConfirm={onConfirm}
+                isVisible={showDatePicker}
+                mode={"range"}
+                onCancel={onCancel}
+            />
+        </View>
+    );
 };
 
 export default TimeSlot;
