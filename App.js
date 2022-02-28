d7953cd0e7edc12024410cdaa7a4ca7ffd3d385b
achiyazigi <matanelcohen0@walla.com>
2022-02-28 16:21:49 +0200

    changed for pr

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { LogBox } from 'react-native';
 import { logHelloWorld } from './config/firebase';
 
 LogBox.ignoreLogs(['Setting a timer']);
+//hi
 LogBox.ignoreLogs(['AsyncStorage']);
 
 export default function App() {
