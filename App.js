cd90f34f1488c9a4e4ff52e42c40a1dfb2151d3a
achiyazigi <matanelcohen0@walla.com>
2022-03-02 17:25:51 +0200

    finished davids demo, notation deleted

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { LogBox } from 'react-native';
 import { logHelloWorld } from './config/firebase';
 
 LogBox.ignoreLogs(['Setting a timer']);
-//hi
 LogBox.ignoreLogs(['AsyncStorage']);
 
 export default function App() {
